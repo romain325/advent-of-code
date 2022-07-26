b3d0d25845997d3af927a5ba72800d2d7f19cef7
romain325 <romainolivier42@gmail.com>
2022-07-26 18:56:31 +0200

    d6 - 2

--- 2021/day6/ex.py
+++ 2021/day6/ex.py
@@ -3,16 +3,36 @@
 n = 80
 
 f = open("./input1")
+
+# bruteforce
+
 line = f.readline()
 arr = [int(i) for i in line.split(",")]
 
 
-for i in range(n):
-    for j in range(len(arr)):
-        if arr[j] == 0:
-            arr[j] = 6
-            arr.append(8)
-        else:
-            arr[j] -= 1
+def brute():
+    for i in range(n):
+        for j in range(len(arr)):
+            if arr[j] == 0:
+                arr[j] = 6
+                arr.append(8)
+            else:
+                arr[j] -= 1
+    print(len(arr))
 
-print(len(arr))
+
+def smart():
+    val = [0] * 9  # zeros arr
+    for v in arr:
+        val[v] += 1
+
+    for i in range(256):
+        current = i % 9
+        val[(current + 7) % 9] += val[current]
+
+    print(sum(val))
+
+
+# brute()
+
+smart()
